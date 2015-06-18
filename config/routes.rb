ac4b40731feac2054094a2b4a037d112a2a0535f
Thanh Nga <thanhngabk1@gmail.com>
2015-06-18 13:53:24 +0700

    log in and log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
   root "static_pages#home"
-  
   get "help" => "static_pages#help"
   get "signup" => "users#new"
-  get "edit" => "users#edit"
+  get "login" => "sessions#new"
+  post "login" => "sessions#create"
+  delete "logout" => "sessions#destroy"
 
   resources :users, except: [:index, :destroy]
 end
