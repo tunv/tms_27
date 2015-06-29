8b3253182404de382bb976d27f418a8574e48d2d
tunv <nguyenvantu.dhbk@gmail.com>
2015-06-29 23:55:04 +0700

    add subject activities to user's actitvity

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -4,5 +4,6 @@ class CoursesController < ApplicationController
   def show
     @course = Course.find params[:id]
     @activities = current_user.activities.course_activities @course.id
+    @subject = @course.subjects
   end
 end
