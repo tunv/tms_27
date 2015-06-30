b1dc840d167e9518fb86949083e0c92ceba3447a
tunv <nguyenvantu.dhbk@gmail.com>
2015-06-30 11:00:48 +0700

    Add task activity to user's activity

--- app/models/user_task.rb
+++ app/models/user_task.rb
@@ -1,4 +1,14 @@
 class UserTask < ActiveRecord::Base
   belongs_to :user
   belongs_to :task
+
+  validates :user_id, presence: true
+  validates :task_id, presence: true
+
+  after_create :set_task_activity
+  
+  private
+  def set_task_activity
+    user.activities.find_or_create_by target_id: task_id, content: Settings.activity.done
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   resources :users, except: [:new, :create, :destroy]
   resources :courses, only: :show
   resources :user_subjects, except: [:index, :destroy]
+  resources :user_tasks, except: [:index, :destroy]
+
   namespace :admin do
     root  "users#index"
     resources :users
